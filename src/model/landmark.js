875b888c8fdc37c907d137eaa5bd2681aad8a790
Andy Anderson <andyanderson1818@gmail.com>
2017-09-18 10:36:51 -0700

    Updated our schema to include coordinates

--- src/model/landmark.js
+++ src/model/landmark.js
@@ -1,8 +1,19 @@
 import mongoose from 'mongoose';
+import Review from './review';
+
 let Schema = mongoose.Schema;
 
 let landMarkSchema = new Schema ({
-  name: String,
+  name: {
+    type: String,
+    required:true
+},
+  avgcost: Number,
+  geometry: {
+    type: { type: String, default: 'Point' },
+    coordinates: [Number]
+  },
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review'}]
 });
 
 module.exports = mongoose.model('Landmark', landMarkSchema);
---
+++ src/model/review.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import LandMark from './landmark';
+let Schema = mongoose.Schema;
+
+let ReviewSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  landmark: {
+    type: Schema.Types.ObjectId,
+    ref: 'Landmark',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
